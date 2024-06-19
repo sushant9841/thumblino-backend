8e68225df525ebd8276bdf562b316d1c4a57d3bf
sushant9841 <43410726+sushant9841@users.noreply.github.com>
2024-06-19 16:29:29 +0545

    update

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require("express");
+const http = require("http");
 const puppeteer = require("puppeteer");
 const cors = require("cors");
 
 const app = express();
-
-// Enable CORS for all origins
 app.use(cors());
 
 let browserInstance = null;
@@ -68,7 +67,7 @@ app.get(
   }
 );
 
-const port = process.env.PORT || 5000;
+const port = 5000;
 app.listen(port, () =>
   console.log(`Server running on http://localhost:${port}`)
 );
